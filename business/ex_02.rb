13bb04f9f62900a5f185c9a56e620735138dc5eb
mauro <mauro_biazutti@hotmail.com>
2023-03-20 20:41:39 -0300

    rails install

--- business/ex_02.rb
+++ business/ex_02.rb
@@ -1,6 +1,6 @@
 # 2 - Em um sistema web, cada usuário pode se inscrever em diversos eventos, cada
 # evento possui até 3 prêmios que são distribuídos entre os usuários presentes.
-#   O sistema precisa guardar a informação sobre quem participou de cada evento e quem ganhou os prêmios.
-#   Modele um banco de dados capaz de suportar essas regras.
+# O sistema precisa guardar a informação sobre quem participou de cada evento e quem ganhou os prêmios.
+# Modele um banco de dados capaz de suportar essas regras.
 
 

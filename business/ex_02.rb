e3b6219861c457fa224c53971b28a66c1edef898
mauro <mauro_biazutti@hotmail.com>
2023-03-21 23:33:39 -0300

    ajustes

--- business/ex_02.rb
+++ business/ex_02.rb
@@ -2,5 +2,3 @@
 # evento possui até 3 prêmios que são distribuídos entre os usuários presentes.
 # O sistema precisa guardar a informação sobre quem participou de cada evento e quem ganhou os prêmios.
 # Modele um banco de dados capaz de suportar essas regras.
-
-

13044e92fbb75a221a0838a6aaae947e32dc5358
mauro <mauro_biazutti@hotmail.com>
2023-03-31 17:25:26 -0300

    exe_06

---
+++ business/ex_06.rb
@@ -0,0 +1,9 @@
+# A estrutura padrão de arquivos de um projeto feito com Ruby on Rails segue a convenção sobre configuração,
+# que significa que a estrutura de pastas e arquivos segue um padrão pré-definido para que os
+# desenvolvedores possam se concentrar no código da aplicação em vez de se preocupar com a configuração.
+#
+#   A seguir estão as pastas onde os models, controllers e views de um projeto devem ser encontrados:
+#
+#   Os models devem ser colocados na pasta app/models.
+#   Os controllers devem ser colocados na pasta app/controllers.
+#   As views devem ser colocadas na pasta app/views.

76f2a210282b4511e8cba87eafd41cbce6375b16
mauro <mauro_biazutti@hotmail.com>
2023-03-31 17:21:32 -0300

    exe_04

---
+++ business/ex_04.rb
@@ -0,0 +1,39 @@
+# A arquitetura MVC (Model-View-Controller) é uma abordagem popular para desenvolvimento de software,
+# que separa as preocupações de apresentação, processamento de dados e controle de fluxo em três componentes distintos.
+# A seguir estão algumas vantagens e desvantagens do uso da arquitetura MVC:
+#
+#                                                                                                                                                                                                                                                                                                   Vantagens:
+#
+#   Separação de preocupações: A arquitetura MVC separa as responsabilidades de apresentação,
+#   processamento de dados e controle de fluxo em componentes distintos.
+#   Isso permite que cada componente seja desenvolvido e testado de forma independente,
+#   o que pode aumentar a qualidade e a facilidade de manutenção do código.
+#
+#   Reutilização de código: Com a separação clara de responsabilidades,
+#   é possível reutilizar componentes de maneira eficiente.
+#   Por exemplo, a mesma lógica de processamento de dados pode ser usada em diferentes visualizações ou interfaces de usuário.
+#
+#   Maior escalabilidade: A separação de preocupações e a reutilização de código também tornam
+#   a arquitetura MVC mais escalável. À medida que o sistema cresce,
+#   é possível adicionar novas visualizações e controladores sem afetar a lógica de processamento de dados subjacente.
+#
+#   Desvantagens:
+#
+#   Complexidade: A separação de preocupações em componentes distintos pode tornar a arquitetura MVC mais complexa e
+#   difícil de entender para desenvolvedores inexperientes.
+#   Além disso, pode haver uma sobrecarga de comunicação entre os componentes.
+#
+#   Overhead de código: A implementação da arquitetura MVC pode exigir mais código e
+#   arquivos do que outras abordagens mais simples. Isso pode aumentar a sobrecarga e o tempo de desenvolvimento.
+#
+#   Dificuldade de aprendizado: A curva de aprendizado para desenvolver em arquitetura MVC pode ser íngreme,
+#   especialmente para desenvolvedores novatos. É preciso aprender as regras de interação entre os componentes e
+#   como lidar com a comunicação entre eles.
+#
+#   Em resumo, a arquitetura MVC pode fornecer muitos benefícios,
+#   como a separação de preocupações e a reutilização de código.
+#   No entanto, também pode ter desvantagens, como complexidade e sobrecarga de código.
+#
+#   Como em qualquer abordagem de desenvolvimento de software,
+#   é importante avaliar cuidadosamente as vantagens e desvantagens e escolher a
+#   abordagem que melhor atenda às necessidades do projeto.

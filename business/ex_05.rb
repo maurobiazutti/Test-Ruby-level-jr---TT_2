7422ec0b7fdd7718eb1075cd21451c6516bde96e
mauro <mauro_biazutti@hotmail.com>
2023-04-03 19:36:40 -0300

    exe_05

--- business/ex_05.rb
+++ business/ex_05.rb
@@ -1,1 +1,10 @@
+#controller com o nome UsersController.
+#método chamado find_user
+class UsersController < ApplicationController
+  def find_user
+    # código para buscar um usuário específico
+  end
+end
+
+
 

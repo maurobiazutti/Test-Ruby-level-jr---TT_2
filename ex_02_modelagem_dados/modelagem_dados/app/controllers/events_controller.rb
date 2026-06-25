5cb0c3bd3472f61d71191d4b007f23a3362313c3
Mauro <mauro_biazutti@hotmail.com>
2026-06-24 22:29:46 -0300

    feat: criando bustas no controller para events

--- ex_02_modelagem_dados/modelagem_dados/app/controllers/events_controller.rb
+++ ex_02_modelagem_dados/modelagem_dados/app/controllers/events_controller.rb
@@ -1,2 +1,12 @@
 class EventsController < ApplicationController
+  def show
+    @event = Event.find(params[:id])
+    
+    # Busca os prêmios do evento
+    @prizes = @event.prizes
+    
+    # Separa os usuários usando os escopos da tabela intermediária
+    @present_users = @event.users.where(event_participations: { present: true })
+    @absent_users  = @event.users.where(event_participations: { present: false })
+  end
 end
